c24287167563fa723c2c47ff033d8084f9204506
EC2 Default User <ec2-user@ip-172-31-94-17.ec2.internal>
2020-04-05 18:11:47 +0000

    changes in config.js for app client id and pool id etc

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_65q1H1MiS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '75p5ju3td3uo2t812fudadfaun', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
